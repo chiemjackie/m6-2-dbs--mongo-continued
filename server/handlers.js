33df6e48b93f71a074536f2b21a44efb8d18adf7
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-08-18 14:07:10 -0400

    Exercise 2

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,30 @@
-'use strict';
+"use strict";
 
-const getSeats = async (req, res) => {};
+const { MongoClient } = require("mongodb");
+const assert = require("assert");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const getSeats = async (req, res) => {
+  try {
+    const client = await MongoClient(MONGO_URI, { useUnifiedTopology: true });
+
+    await client.connect();
+
+    const database = client.db("seat_booking");
+
+    const seatsData = await database.collection("seats").find().toArray();
+
+    return seatsData.reduce((seats, seatData) => {
+      const { _id, price, isBooked } = seatData;
+
+      seats[_id] = { price, isBooked };
+      return seats;
+    }, {});
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 module.exports = { getSeats };
--- server/routes.js
+++ server/routes.js
@@ -1,22 +1,11 @@
 const router = require("express").Router();
+const { getSeats } = require("./handlers");
+
+let state;
 
 const NUM_OF_ROWS = 8;
 const SEATS_PER_ROW = 12;
 
-// Code that is generating the seats.
-// ----------------------------------
-const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-for (let r = 0; r < row.length; r++) {
-  for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      price: 225,
-      isBooked: false,
-    };
-  }
-}
-// ----------------------------------
-//////// HELPERS
 const getRowName = (rowIndex) => {
   return String.fromCharCode(65 + rowIndex);
 };
@@ -38,21 +27,24 @@ const randomlyBookSeats = (num) => {
   return bookedSeats;
 };
 
-let state;
-
 router.get("/api/seat-availability", async (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
+  try {
+    if (!state) {
+      state = {
+        bookedSeats: randomlyBookSeats(30),
+      };
+    }
 
-  return res.json({
-    seats: seats,
-    bookedSeats: state.bookedSeats,
-    numOfRows: 8,
-    seatsPerRow: 12,
-  });
+    return res.json({
+      seats: await getSeats(),
+      bookedSeats: state.bookedSeats,
+      numOfRows: 8,
+      seatsPerRow: 12,
+    });
+  } catch ({ message }) {
+    console.log(message);
+    res.status(404).json({ status: 404, message });
+  }
 });
 
 let lastBookingAttemptSucceeded = false;
